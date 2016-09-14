c0431a12c8fec7480f7d3a97ba28fd0b17c15463
Rob Cornell <rob.cornell@gmail.com>
2016-09-13 22:17:48 -0400

    Creating Position class but have to talk to my better half.

--- Asset Management Platform/Portfolio.cs
+++ Asset Management Platform/Portfolio.cs
@@ -8,5 +8,6 @@ namespace Asset_Management_Platform.ViewModel
 {
     class Portfolio
     {
+
     }
 }
---
+++ Asset Management Platform/Position.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asset_Management_Platform
+{
+    class Position
+    {
+        private Stock stockInfo;
+        public Stock StockInfo { get { return stockInfo; } }
+
+        private long sharesOwned;
+        public long SharesOwned { get { return sharesOwned; } }
+
+        //public float Value {
+         //   get { return float.Parse((stockInfo.LastPrice * sharesOwned).ToString); } }
+
+        public Position(Stock stock, long shares)
+        {
+
+        }
+    }
+}
