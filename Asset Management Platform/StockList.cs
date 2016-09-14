52dc965d3107c8f056022ac732cd40e958d2b096
Rob Cornell <rob.cornell@gmail.com>
2016-09-13 22:05:33 -0400

    Added basic properties to StockList class

--- Asset Management Platform/StockList.cs
+++ Asset Management Platform/StockList.cs
@@ -8,10 +8,44 @@ namespace Asset_Management_Platform.ViewModel
 {
     class StockList
     {
+
+        private List<Stock> stocks;
+
         public StockList()
         {
-
+            stocks = GenerateStockList();
         }
+
+        public List<Stock> GenerateStockList()
+        {
+            var list = new List<Stock>();
+
+            //DoStuff
+            //With StockDataService Probably
+
+            return list;
+        }
+
+        public void AddStockToList(string name,
+            string ticker,
+            decimal lastPrice,
+            decimal bid,
+            decimal ask,
+            decimal beta,
+            decimal peRatio,
+            long volume,
+            long bidSize,
+            long askSize)
+        {
+            stocks.Add(new Stock(name, ticker, lastPrice, bid, ask, beta, peRatio, volume, bidSize, askSize));
+        }
+
+        public void RemoveStockFromList(Stock stockToRemove)
+        {
+            stocks.Remove(stockToRemove);
+        }
+
+
 
     }
 }
