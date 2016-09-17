3b55ac137bd64880e9a4a82dce8d4e35d109e858
Rob Cornell <rob.cornell@gmail.com>
2016-09-17 01:39:18 -0400

    Sketched out methods and property of Portfolio class.

--- Asset Management Platform/Portfolio.cs
+++ Asset Management Platform/Portfolio.cs
@@ -8,6 +8,41 @@ namespace Asset_Management_Platform.ViewModel
 {
     class Portfolio
     {
+        private List<Security> _myPortfolio;
+        public List<Security> MyPortfolio
+        {
+            get { return _myPortfolio; }
+            set { _myPortfolio = value; }
+        }
 
+        public Portfolio()
+        {
+            LoadPortfolio();
+        }
+
+        private void LoadPortfolio()
+        {
+            try { }
+            catch { }
+            //Pull portfolio from Database?
+        }
+
+
+        private void SavePortfolio()
+        {
+            try { }
+            catch { }
+        }
+
+        private bool AddToPortfolio(Security securityToAdd)
+        {
+            return true;
+        }
+
+        private bool RemoveFromPortfolio(Security securityToRemove)
+        {
+            return true;
+        }
     }
+
 }
