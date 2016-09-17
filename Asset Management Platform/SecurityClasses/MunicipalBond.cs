ab01d4699fc1b1fc041486517c13bf9bde802e0c
Rob Cornell <rob.cornell@gmail.com>
2016-09-17 01:09:13 -0400

    Created MunicipalBond, CorporateBond, and Treasury, which derive from FixedIncome

---
+++ Asset Management Platform/SecurityClasses/MunicipalBond.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asset_Management_Platform.SecurityClasses
+{
+    class MunicipalBond : FixedIncome
+    {
+        public MunicipalBond(string cusip, string ticker, string description, double lastPrice, double yield) 
+            : base(cusip, ticker, description, lastPrice, yield)
+        {
+
+        }
+
+        public MunicipalBond(string cusip, string ticker, string description, double lastPrice, double yield, double coupon, string issuer, string rating)
+            : base(cusip, ticker, description, lastPrice, yield, coupon, issuer, rating)
+        {
+
+        }
+    }
+}
