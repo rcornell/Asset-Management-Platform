0f7b831c056438a941a87e52d4ec99f8ba81e27d
Rob Cornell <rob.cornell@gmail.com>
2016-09-23 00:45:08 -0400

    Sketched fields and properties in PortfolioService

--- Asset Management Platform/Utility/PortfolioService.cs
+++ Asset Management Platform/Utility/PortfolioService.cs
@@ -4,17 +4,44 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using GalaSoft.MvvmLight.Ioc;
+using System.Windows.Threading;
 
 namespace Asset_Management_Platform.Utility
 {
     class PortfolioService
     {
-        private StockDataService stockValue;
+        private StockDataService _stockValue;
+        private DispatcherTimer _timer;
+        private List<Security> _securityList;
 
-        public PortfolioService()
+        public PortfolioService(List<Security> securityList)
         {
-            stockValue = SimpleIoc.Default.GetInstance<StockDataService>();
+            _stockValue = SimpleIoc.Default.GetInstance<StockDataService>();
+            _securityList = securityList;
+            _timer = new DispatcherTimer();
+            _timer.Interval = new TimeSpan(0, 0, 10);           
+            CalculatePositionValues();
         }
 
+        private void CalculatePositionValues()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Starts the 10-second-interval update timer
+        /// </summary>
+        public void StartUpdates()
+        {
+            _timer.Start();
+        }
+
+        /// <summary>
+        /// Stops the 10-second-interval update timer
+        /// </summary>
+        public void StopUpdates()
+        {
+            _timer.Stop();
+        }
     }
 }
