ebbd95dacdc96d90e0a0eee91aff2f305a001e26
Rob Cornell <rob.cornell@gmail.com>
2016-12-22 22:09:07 -0500

    Beginning of chart implementation. Created PositionByWeight class. This is utilized in an ObservableCollection in MainViewModel for data binding to the chart in UI. A new MainViewModel method, GetAllocationChartPositions(), build the collection with associated new properties.

---
+++ Asset Management Platform/PositionByWeight.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asset_Management_Platform
+{
+    public class PositionByWeight
+    {
+
+        public string Ticker;
+
+        public decimal Weight;
+
+        public PositionByWeight(string ticker, decimal weight)
+        {
+            Ticker = ticker;
+            Weight = weight;
+        }
+    }
+}
