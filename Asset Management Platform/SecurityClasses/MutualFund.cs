b1df54f672698d6b1fdce83070ec1b5f37172ed1
Rob Cornell <rob.cornell@gmail.com>
2016-12-23 23:22:15 -0500

    Security now inherits from ObservableObject to implement RaisePropertyChanged on its properties. Mutual fund class starting to be expanded.

--- Asset Management Platform/SecurityClasses/MutualFund.cs
+++ Asset Management Platform/SecurityClasses/MutualFund.cs
@@ -9,6 +9,35 @@ namespace Asset_Management_Platform
     class MutualFund : Security
     {
 
+        private string _assetClass;
+        public string AssetClass
+        {
+            get { return _assetClass; }
+            set
+            {
+                _assetClass = value;
+                RaisePropertyChanged(() => AssetClass);
+            }
+        }
+
+        private string _category;
+        public string Category
+        {
+            get { return _category; }
+            set
+            {
+                _category = value;
+                RaisePropertyChanged(() => Category);
+            }
+        }
+
+        private string _subcategory;
+        public string Subcategory {
+            get { return _subcategory; }
+            set { _subcategory = value;
+                RaisePropertyChanged(() => Subcategory);
+            }
+        }
 
 
         public MutualFund(string cusip, string ticker, string description, decimal lastPrice, double yield)
--- Asset Management Platform/SecurityClasses/Security.cs
+++ Asset Management Platform/SecurityClasses/Security.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using GalaSoft.MvvmLight;
 
 namespace Asset_Management_Platform
 {
-    public class Security
+    public class Security : ObservableObject
     {
         public string SecurityType;
 
@@ -14,35 +15,45 @@ namespace Asset_Management_Platform
         public string Cusip
         {
             get { return _cusip; }
-            set { _cusip = value; }
+            set { _cusip = value;
+                RaisePropertyChanged(() => Cusip);
+            }
         }
 
         private string _ticker;
         public string Ticker
         {
             get { return _ticker; }
-            set { _ticker = value; }
+            set { _ticker = value;
+                RaisePropertyChanged(() => Ticker);
+            }
         }
 
         private string _description;
         public string Description
         {
             get { return _description; }
-            set { _description = value; }
+            set { _description = value;
+                RaisePropertyChanged(() => Description);
+            }
         }
 
         private decimal _lastPrice;
         public decimal LastPrice
         {
             get { return _lastPrice; }
-            set { _lastPrice = value; }
+            set { _lastPrice = value;
+                RaisePropertyChanged(() => LastPrice);
+            }
         }
 
         private double _yield;
         public double Yield
         {
             get { return _yield; }
-            set { _yield = value; }
+            set { _yield = value;
+                RaisePropertyChanged(() => Yield);
+            }
         }
 
         public Security(string cusip, string ticker, string description, decimal lastPrice, double yield)
