faa07b1e1cb046fe58678a762abcd43a24868693
Rob Cornell <rob.cornell@gmail.com>
2017-02-03 00:04:52 -0500

    Create first draft of BoolToVisibilityConverter
    This will control the "Order Terms are Ok" box's appearance.

--- Asset Management Platform/Converters/BoolToVisibilityConverter.cs
+++ Asset Management Platform/Converters/BoolToVisibilityConverter.cs
@@ -4,23 +4,26 @@ using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Data;
 using System.Windows;
+using System.Windows.Data;
 
 namespace Asset_Management_Platform.Converters
 {
-    class BoolToOpacityConverter : IValueConverter
+    public class BoolToVisibilityConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-
-            return "";
-            
+            if (value is bool && (bool)value == true)
+                return Visibility.Visible;
+            else
+                return Visibility.Hidden;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            throw new NotImplementedException();
+            if (value is Visibility && (Visibility)value == Visibility.Hidden)
+                return false;
+            return true;
         }
     }
 }
