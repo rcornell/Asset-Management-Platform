c0bb924f6bf25d0f17725a0cf8b4a859004aeb1b
Rob Cornell <rob.cornell@gmail.com>
2017-02-11 14:42:17 -0500

    Add DisplayStock UI-specific properties to Position

--- Asset Management Platform/Position.cs
+++ Asset Management Platform/Position.cs
@@ -3,11 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using GalaSoft.MvvmLight;
 
 namespace Asset_Management_Platform
 {
-    public class Position
+    public class Position : ObservableObject
     {
+
+        private Stock _stock;
+        public Stock Stock
+        {
+            get
+            {
+                return _stock;
+            }
+            set
+            {
+                _stock = value;
+            }
+        }
+
+        public string Description
+        {
+            get { return Stock.Description; }
+        }
+
+        public decimal Price
+        {
+            get { return Stock.LastPrice; }
+        }
+
         private string _ticker;
         public string Ticker {
             get { return _ticker;  }
@@ -35,11 +60,81 @@ namespace Asset_Management_Platform
             get { return ComputePurchasePrice(); }
         }
 
+        public string MarketValue
+        {
+            get
+            {
+                var value = Stock.LastPrice * SharesOwned;
+                var valueString = value.ToString("#,##0");
+                return valueString;
+            }
+        }
+
+        public string Yield
+        {
+            get { return string.Format(Stock.Yield + "%"); }
+        }
+
         private List<Taxlot> _taxlots;
         public List<Taxlot> Taxlots
         {
             get { return _taxlots; } 
             set { _taxlots = value; }
+        }
+
+        public double Bid
+        {
+            get { return Stock.Bid; }
+        }
+
+        public double Ask
+        {
+            get { return Stock.Ask; }
+        }
+
+        public string MarketCap
+        {
+            get
+            {
+                var stringChars = Stock.MarketCap.ToString().Split('.');
+                var newString = stringChars[0] + " Billion";
+                return newString;
+            }
+        }
+
+        public double PeRatio
+        {
+            get { return Stock.PeRatio; }
+        }
+
+        public double AskSize
+        {
+            get { return Stock.AskSize; }
+        }
+
+        public double BidSize
+        {
+            get { return Stock.BidSize; }
+        }
+
+        public string Volume
+        {
+            get { return Stock.Volume.ToString("#,##0"); }
+        }
+
+        public string GainLoss
+        {
+            get { return ((Stock.LastPrice - PurchasePrice) * SharesOwned).ToString("#,##0"); }
+        }
+
+        public decimal Change
+        {
+            get { return Stock.Change; }
+        }
+
+        public string PercentChange
+        {
+            get { return string.Format(@"{0:0.0%}", Stock.PercentChange / 100); }
         }
 
         public Position(List<Taxlot> taxlots)
