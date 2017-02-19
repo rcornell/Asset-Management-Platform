cab69d86d8dc1605b78ed104e6df8c46c74f1b61
Rob Cornell <rob.cornell@gmail.com>
2017-02-19 17:24:01 -0500

    Implement TryLoadPortfolio with async and try/catch

--- Asset Management Platform/Utility/PortfolioFileOps.cs
+++ Asset Management Platform/Utility/PortfolioFileOps.cs
@@ -12,9 +12,11 @@ namespace Asset_Management_Platform.Utility
 {
     public class PortfolioFileOps : IDisposable
     {
+        private string documentsPath;
+
         public PortfolioFileOps()
         {
-            
+            documentsPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
         }
 
         public bool TrySaveTaxlots(ObservableCollection<Position> positions)
@@ -26,7 +28,6 @@ namespace Asset_Management_Platform.Utility
         }
         public async Task<bool> TrySaveTaxlots(ObservableCollection<Taxlot> taxlots)
         {
-            var documentsPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
             var dialog = new SaveFileDialog
             {
                 InitialDirectory = documentsPath,
@@ -46,18 +47,80 @@ namespace Asset_Management_Platform.Utility
                 return false;
 
             var path = dialog.FileName;
-            var jsonString = await Task.Factory.StartNew(() => JsonConvert.SerializeObject(taxlots));
-            File.WriteAllText(path, jsonString);
+            try
+            {
+                var jsonString = await Task.Factory.StartNew(() => JsonConvert.SerializeObject(taxlots));
+                File.WriteAllText(path, jsonString);
+                return true;
+            }
+            catch (ArgumentException ex)
+            {
 
-            return true;
+            }
+            catch (FileNotFoundException ex)
+            {
+
+            }
+            catch (IOException ex)
+            {
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return false;
         }
 
-        public List<Taxlot> TryLoadPortfolio()
+        public async Task<ObservableCollection<Taxlot>> TryLoadPortfolio()
         {
-            var taxlotsToReturn = new List<Taxlot>();
+            var taxlotsToReturn = new ObservableCollection<Taxlot>();
 
+            var dialog = new OpenFileDialog()
+            {
+                InitialDirectory = documentsPath,
+                CheckFileExists = true,
+                CheckPathExists = true,
+                AddExtension = true,
+                DefaultExt = ".json",
+                FileName = "MyPortfolio",
+                Filter = "JSON|*.json",
+                Title = @"Choose a portfolio to load."
+            };
 
+            bool? loadPathConfirmed = dialog.ShowDialog();
 
+            if (loadPathConfirmed == null || !loadPathConfirmed.Value)
+                return taxlotsToReturn;
+
+            var path = dialog.FileName;
+
+            try
+            {
+                var result = File.ReadAllText(path);
+                var taxlotList =
+                    await Task.Factory.StartNew(
+                        () => JsonConvert.DeserializeObject<ObservableCollection<Taxlot>>(result));
+
+                taxlotsToReturn = taxlotList;
+                return taxlotsToReturn;
+            }
+            catch (ArgumentException ex)
+            {
+
+            }
+            catch (FileNotFoundException ex)
+            {
+
+            }
+            catch (IOException ex)
+            {
+
+            }
+            catch (Exception ex)
+            {
+                
+            }
             return taxlotsToReturn;
         }
 
