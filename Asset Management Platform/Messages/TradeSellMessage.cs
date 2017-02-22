76311ec9b018d40403df257499813cc93e774ef7
Rob Cornell <rob.cornell@gmail.com>
2017-02-21 23:22:16 -0500

    Create TradeSellMessage and TradeBuyMessage
    These will be used to send trade data

---
+++ Asset Management Platform/Messages/TradeSellMessage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asset_Management_Platform.Messages
+{
+    public class TradeSellMessage
+    {
+        public Trade Trade;
+
+        public TradeSellMessage(Trade trade)
+        {
+            Trade = trade;
+        }
+    }
+}
