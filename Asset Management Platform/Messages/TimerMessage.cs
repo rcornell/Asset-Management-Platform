5f2eb075a369e7e6311a457e913496605a8861a1
Rob Cornell <rob.cornell@gmail.com>
2017-02-22 22:14:27 -0500

    Create TimerMessage class

---
+++ Asset Management Platform/Messages/TimerMessage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asset_Management_Platform.Messages
+{
+    public class TimerMessage
+    {
+        public bool StartTimer;
+        public bool StopTimer;
+        public TimeSpan Span;
+
+        public TimerMessage(TimeSpan span, bool startTimer, bool stopTimer)
+        {
+            Span = span;
+            StartTimer = startTimer;
+            StopTimer = stopTimer;
+        }
+    }
+}
